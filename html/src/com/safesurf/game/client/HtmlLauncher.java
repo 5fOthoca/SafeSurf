ddc1ad38190815d9481a7763da19c9e3425f4fc2
AngeloMarras <marras.itis@gmail.com>
2015-04-01 12:50:02 +0200

    versione 0.0

---
+++ html/src/com/safesurf/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.safesurf.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.safesurf.game.MySafeSurfGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MySafeSurfGame();
+        }
+}
